f89fb1c5f8989aa3818044600b96964977149027
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-08-27 21:04:36 +0530

    transactions page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,14 @@
-const Dashboard = () => {
+import Transactions from "../ui/transactions/transactions"
+import styles from '@/app/ui/dashboard/dashboard.module.css'
+
+const Dashbaord = () => {
   return (
-    <div>
-        Dashboard
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+      <Transactions/>
+      </div>  
     </div>
   )
 }
 
-export default Dashboard
+export default Dashbaord
